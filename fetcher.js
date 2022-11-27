d6643a2dbb139cfeac40c7305f8b597709e1d090
jrivera604 <joshrivera91@hotmail.com>
2022-11-27 22:15:15 +0000

    create fetcher function to download file from site to local machine

---
+++ fetcher.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const fs = require('fs');
+//installed npm pakage to determine if path is valid
+const validPath = require('is-valid-path')
+const input = process.argv.slice(2);
+const url = input[0];
+const path = input[1];
+
+const fetcher = () => {
+  request(url, (error, response, body) => {
+    if(error){
+      console.log(`Critical Error: ${error}`)
+      process.exit();
+    }
+    if(response){
+      fs.writeFile(path, body, (error) => {
+        if(error){
+          throw error;
+        }
+        else{ 
+          console.log(`Downloaded ${body.length} bytes to ${path}`);
+      }
+      })
+    }
+  })
+  if(!validPath(path)){
+    console.log(`${path} is not a valid path`)
+    process.exit();
+  }
+
+}
+
+fetcher();
